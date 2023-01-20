5d2507475c739bd67b4180c3271912b4f3d5366e
Eric Hu <eric-hu@users.noreply.github.com>
2023-01-19 18:15:48 -0800

    gofmt

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-  "fmt"
-  "example/user/hello/morestrings"
+	"example/user/hello/morestrings"
+	"fmt"
 )
 
 func main() {
-  fmt.Println(morestrings.ReverseRunes("!dlrow olleH"))
-  fmt.Println(morestrings.ReverseRunes("!界世好你"))
-  fmt.Println(morestrings.ReverseRunes("!🧑‍🍳"))
-  fmt.Println(morestrings.ReverseRunes("!🍳‍🧑"))
+	fmt.Println(morestrings.ReverseRunes("!dlrow olleH"))
+	fmt.Println(morestrings.ReverseRunes("!界世好你"))
+	fmt.Println(morestrings.ReverseRunes("!🧑‍🍳"))
+	fmt.Println(morestrings.ReverseRunes("!🍳‍🧑"))
 }
-
